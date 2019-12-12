247794378be5c47a10ee84949a92b5cf764bed83
zhaoyi <yizhao0328@gmail.com>
2019-12-12 19:02:46 +0800

    log modify

--- guava/src/main/java/com/soup/memo/guava/utils/StopWatchDemo.java
+++ guava/src/main/java/com/soup/memo/guava/utils/StopWatchDemo.java
@@ -1,8 +1,7 @@
 package com.soup.memo.guava.utils;
 
 import com.google.common.base.Stopwatch;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -15,19 +14,19 @@ import java.util.concurrent.TimeUnit;
  * @author zhaoyi
  * @date 2019-03-28 10:57
  */
+@Slf4j
 public class StopWatchDemo {
 
-    private static final Logger LOGGER = LogManager.getLogger(StopWatchDemo.class);
 
     public static void main(String[] args) throws Exception {
         process(new Random(10).nextInt());
     }
 
     private static void process(int orderNo) throws InterruptedException {
-        LOGGER.info("start process the order [{}]", orderNo);
+        log.info("start process the order [{}]", orderNo);
         Stopwatch stopwatch = Stopwatch.createStarted();
         TimeUnit.MICROSECONDS.sleep(new Random(10).nextInt());
-        LOGGER.info("the order [{}] processs successful and elapsed [{}] minutes.", orderNo,
+        log.info("the order [{}] processs successful and elapsed [{}] minutes.", orderNo,
                         stopwatch.stop().elapsed(TimeUnit.MINUTES));
 
     }
