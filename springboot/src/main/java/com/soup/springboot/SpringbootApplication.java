692b41039799e22d44eaa29535a6e7203cbe2985
zhaoyi <yizhao0328@gmail.com>
2019-12-30 22:16:06 +0800

    print classloader

--- springboot/src/main/java/com/soup/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/soup/springboot/SpringbootApplication.java
@@ -1,5 +1,6 @@
 package com.soup.springboot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,10 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * springboot 启动类
  * @author zhaoyi
  */
+@Slf4j
 @SpringBootApplication
 public class SpringbootApplication {
 
     public static void main(String[] args) {
+
+        // 打印类加载器
+        log.info("加载 SpringbootApplication classLoader 是: {}", SpringbootApplication.class.getClassLoader());
+
         SpringApplication.run(SpringbootApplication.class, args);
     }
 
