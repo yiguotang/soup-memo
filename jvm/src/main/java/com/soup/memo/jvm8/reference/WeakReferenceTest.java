4bcd701b88597bc5170b1b69858542ebe4526afb
zhaoyi <yizhao0328@gmail.com>
2020-03-02 23:25:36 +0800

    WeakReference

--- jvm/src/main/java/com/soup/memo/jvm8/reference/WeakReferenceTest.java
+++ jvm/src/main/java/com/soup/memo/jvm8/reference/WeakReferenceTest.java
@@ -1,5 +1,8 @@
 package com.soup.memo.jvm8.reference;
 
+import java.lang.ref.WeakReference;
+import java.util.Date;
+
 /**
  * <p>
  *
@@ -12,6 +15,13 @@ package com.soup.memo.jvm8.reference;
 public class WeakReferenceTest {
 
     public static void main(String[] args) {
+        Date date = new Date();
+        WeakReference<Date> reference = new WeakReference<>(date);
+        System.out.println("立即取出：" + reference.get());
+        date = null;
+        System.out.println("将强引用置null，再次取出：" + reference.get());
 
+        System.gc();
+        System.out.println("执行完gc后，再取出：" + reference.get());
     }
 }
